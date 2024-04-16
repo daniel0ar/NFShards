d06da75d218911c8af326eb0f6c18ee3615658b1
Daniel <daniel0ar@hotmail.com>
2024-04-16 09:16:14 -0500

    feat: Add mint new option for empty nfts owned

--- src/pages/process/SelectNFT.tsx
+++ src/pages/process/SelectNFT.tsx
@@ -1,21 +1,31 @@
 import NFTCard from "@/components/NFTCard";
+import { WalletContext } from "@/context/WalletContext";
 import { useOwnedNFTList } from "@/hooks";
-import React from "react";
+import { Button } from "antd";
+import React, { useContext } from "react";
 
 const SelectNFT = () => {
-  const nfts = useOwnedNFTList("0x33aaDa02c476229251Ae0aa211ea1E1bF125Dd51")
+  const { selectedAddress } = useContext(WalletContext);
+  const nfts = useOwnedNFTList(selectedAddress);
   return (
     <div>
       <div className="mb-10">
-        <h1 className="text-5xl leading-normal font-bold">Select NFT to shard</h1>
-        <p>
-          Which of your NFTs would you like to divide into token fractions?
-        </p>
+        <h1 className="text-5xl leading-normal font-bold">
+          Select NFT to shard
+        </h1>
+        <p>Which of your NFTs would you like to divide into token fractions?</p>
       </div>
       <div className="grid grid-cols-4 ">
-        {nfts.map((nft, index) => (
-          <NFTCard key={index} nft={nft}></NFTCard>
-        ))}
+        {nfts.length > 0 ? (
+          nfts.map((nft, index) => <NFTCard key={index} nft={nft}></NFTCard>)
+        ) : (
+          <div className="text-center col-span-4 py-10">
+            <div className="flex flex-col items-center gap-3">
+              <span className="text-slate-400">No NFTs found</span>
+              <Button type="primary">Mint One</Button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
