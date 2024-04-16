4efdb1cdf34a1dd0848186c80117f3044e67b0b4
Daniel <daniel0ar@hotmail.com>
2024-04-16 10:31:00 -0500

    feat: Show selected NFT info on Details component

--- src/pages/process/ShardDetails.tsx
+++ src/pages/process/ShardDetails.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import { ProcessContext } from '@/context/ProcessContext'
+import React, { useContext } from 'react'
 
 type Props = {}
 
 const ShardDetails = (props: Props) => {
+  const { nftCollectionAddress, nftTokenId } = useContext(ProcessContext)
   return (
     <>
       <div>
         ShardDetails
       </div>
       <div>
-        details
+        <p>Collection Address: {nftCollectionAddress}</p>
+        <p>Token Id: {nftTokenId}</p>
       </div>
     </>
   )
