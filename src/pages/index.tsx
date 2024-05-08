921eddd29e7a56a72bffa0dd52bd997caf30b9d3
Daniel <daniel0ar@hotmail.com>
2024-05-07 23:45:55 -0500

    fix: Typo on img attr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,7 @@ export default function Home() {
               created.
             </p>
           </div>
-          {shards?.length > 0 && <ShardCard name={shards[0]?.[2]} description={shards[0]?.[1]} Image={placeholderImage}></ShardCard>}
+          {shards?.length > 0 && <ShardCard name={shards[0]?.[2]} description={shards[0]?.[1]} img={placeholderImage}></ShardCard>}
           <a
             href="https://Shard.studio/"
             className="ring-1 lg:row-span-2 flex flex-col justify-between group hover:ring-primary/10 dark:hover:ring-white/20 duration-300 h-full dark:ring-white/10 ring-primary/5 lg:row-start-2 md:grid-cols-2 md:grid lg:gap-0 md:items-center md:gap-12 lg:grid-cols-none lg:col-start-2 lg:col-span-2 rounded-3xl p-8 bg-white dark:bg-secondary shadow-xl dark:shadow-thick"
